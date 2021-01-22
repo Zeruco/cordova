d5b2a99f5afd8009f56953b04e41a1049460a357
Zeruco <65422840+Zeruco@users.noreply.github.com>
2021-01-22 08:52:25 +0300

    Delete main.js

--- www/js/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    window.open('http://site.com', '_self');
-    cordova.InAppBrowser.open('http://site.com', '_self');
-}
